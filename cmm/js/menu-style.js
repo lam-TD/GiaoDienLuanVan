47c11937377d509d0d90b581cf72f81c2bbe504d
PhuongQuynh <32539012+lam-TD@users.noreply.github.com>
2018-03-03 07:28:24 +0700

    login

--- cmm/js/menu-style.js
+++ cmm/js/menu-style.js
@@ -19,5 +19,73 @@ document.addEventListener("DOMContentLoaded",function () {
 			flag = false;
 		}
 	});
-		
+
+	// select button
+	var btnSelect = document.getElementsByClassName('btn-select');
+	var aSelect = document.getElementsByClassName('a-select');
+	var andi =  document.getElementsByClassName('select-content');
+
+	for (var i = 0; i < aSelect.length; i++) {
+		var oldAtt = "";
+		aSelect[i].onclick = function(){
+			for (var j = 0; j < aSelect.length; j++) { aSelect[j].classList.remove('click-select'); }
+			var hien_len = this.getAttribute("data-hienthi");
+			
+			if (oldAtt == hien_len) 
+			{
+				this.classList.remove('click-select');
+				oldAtt = "";
+				var phan_hien_len = document.getElementById(hien_len);
+				for (var k = 0; k < andi.length; k++) {
+					andi[k].classList.remove('hienthi');
+				}
+				phan_hien_len.classList.remove('hienthi');
+			}
+			else
+			{
+				this.classList.add('click-select');
+				oldAtt = hien_len;
+				var phan_hien_len = document.getElementById(hien_len);
+				for (var k = 0; k < andi.length; k++) {
+					andi[k].classList.remove('hienthi');
+				}
+				phan_hien_len.classList.add('hienthi');
+			}
+			
+		}
+	}
+
+	// select item TinhTp
+	var select_Item = document.getElementsByClassName('selectItem');
+	for (var i = 0; i < select_Item.length; i++) {
+		select_Item[i].onclick = function () {
+			document.getElementById('a-tinhTP').innerHTML = this.getAttribute('data-name') + ' <i class="fas fa-angle-down"></i>';
+			document.getElementsByClassName('btn-select')[0].classList.remove('click-select');
+			document.getElementsByClassName('a-select')[0].classList.remove('hienthi');
+		}
+	}
+
+	// select item danh muc
+	var select_Item2 = document.getElementsByClassName('selectItem2');
+	for (var i = 0; i < select_Item2.length; i++) {
+		select_Item2[i].onclick = function () {
+			document.getElementById('a-danhmuc').innerHTML = this.getAttribute('data-name') + ' <i class="fas fa-angle-down"></i>';
+			document.getElementsByClassName('btn-select')[1].classList.remove('click-select');
+			document.getElementsByClassName('select-content')[1].classList.remove('hienthi');
+			console.log(document.getElementsByClassName('select-content'));
+		}
+	}
+	
+	// form login
+	var modal = document.getElementById('id01');
+	document.getElementById('btn-dangnhap').onclick = function(){
+		document.getElementById('id01').style.display = "block";
+	}
+	// When the user clicks anywhere outside of the modal, close it
+	window.onclick = function(event) {
+	    if (event.target == modal) {
+	        modal.style.display = "none";
+	    }
+	}
+
 },false);
